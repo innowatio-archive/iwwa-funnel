594b72e95fa58639d31d98350c3c8c05531e52a9
Emanuele Busi <emanuele.busi@mondora.com>
2015-07-03 20:50:47 +0200

    Added logs

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM iojs
 RUN mkdir /iwwa-funnel
 ADD ./ /iwwa-funnel/
 WORKDIR /iwwa-funnel
+RUN mkdir logs
 RUN npm install
 EXPOSE 8012
 ENTRYPOINT ["npm", "start"]
--- app/server.js
+++ app/server.js
@@ -1,7 +1,11 @@
-var baucis   = require("baucis");
-var cors     = require("cors");
-var express  = require("express");
-var mongoose = require("mongoose");
+var baucis            = require("baucis");
+var cors              = require("cors");
+var express           = require("express");
+var mongoose          = require("mongoose");
+var winston           = require("winston");
+var expressWinston    = require("express-winston");
+var R                 = require("ramda");
+require("winston-cloudwatch");
 
 var auth = require("./lib/auth-middleware.js");
 
@@ -9,7 +13,11 @@ var config = {
     MONGO_URL: process.env.MONGO_URL || "mongodb://localhost:27017/iwwa-funnel",
     PASSWORD: process.env.PASSWORD || "PASSWORD",
     PORT: 8012,
-    USERNAME: process.env.USERNAME || "USER"
+    USERNAME: process.env.USERNAME || "USER",
+    LOG_FILE: process.env.LOG_FILE || "./logs/iwwa-funnel.log",
+    LOG_LEVEL: process.env.LOG_LEVEL || "info",
+    LOG_GROUP_NAME: process.env.LOG_GROUP_NAME,
+    LOG_GROUP_STREAM: process.env.LOG_GROUP_STREAM
 };
 
 // Use baucis decorators
@@ -29,12 +37,37 @@ api.releases("1.0.0");
 // Establish mongoose connection
 mongoose.connect(config.MONGO_URL);
 
+expressWinston.requestWhitelist.push("body");
+expressWinston.responseWhitelist.push("body");
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)({
+            json: true,
+            colorize: true
+        }),
+        new (winston.transports.File)({
+            filename: config.LOG_FILE,
+            json: false,
+            level: config.LOG_LEVEL
+        }),
+        (config.LOG_GROUP_NAME && config.LOG_GROUP_STREAM) && new winston.transports.CloudWatch({
+            level: config.LOG_LEVEL,
+            logGroupName: config.LOG_GROUP_NAME,
+            logStreamName: config.LOG_GROUP_STREAM
+        })
+    ].filter(R.identity)
+});
+
 // Start express application
 express()
     .use(cors())
     .use(auth(config.USERNAME, config.PASSWORD))
+    .use(expressWinston.logger({
+        winstonInstance: logger
+    }))
     .use("/swagger/", express.static("node_modules/swagger-ui/dist/"))
     .use("/api/", api)
     .listen(config.PORT, function () {
-        console.log("Server listening on port " + config.PORT);
+        winston.info("Server listening on port " + config.PORT);
     });
